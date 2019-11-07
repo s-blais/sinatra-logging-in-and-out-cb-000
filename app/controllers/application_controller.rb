3bcccf2488e7f00f207ce93c152fc586ab63e707
Stephen Blais <stephen@s-blais.com>
2019-11-07 19:53:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,16 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params["username"], password: params["password"])
-    erb :error if @user == nil
-    session[:user_id] = @user.id
-    redirect '/account' # is "redirect to" needed here?
+    if @user = User.find_by(username: params["username"], password: params["password"])
+      session[:user_id] = @user.id
+      redirect to '/account' # is "redirect to" needed here?
+    else
+      erb :error
+    end
+    # @user = User.find_by(username: params["username"], password: params["password"])
+    # erb :error if @user == nil
+    # session[:user_id] = @user.id
+    # redirect '/account' # is "redirect to" needed here?
   end
 
   get '/account' do
