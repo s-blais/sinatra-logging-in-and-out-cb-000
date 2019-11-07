54bd15bbe5f14d431be232d72491f03f235a9076
Stephen Blais <stephen@s-blais.com>
2019-11-07 22:15:57 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -6,7 +6,7 @@ class Helpers
 
   def self.is_logged_in?(session)
     !!session[:user_id]
-    # I researched the '!!' and what I saw said that it is kinda frowned upon
+    # I researched the '!!' and what I saw said that it is kinda frowned upon, so what do we use instead?
   end
 
 end
