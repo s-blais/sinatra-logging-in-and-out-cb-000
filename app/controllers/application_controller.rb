f7d006f2a986371d11d9877a1502c65a6a8f8729
Stephen Blais <stephen@s-blais.com>
2019-11-07 21:18:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @user = User.find(session[:user_id])
+    @user = User.find_by_id(session[:user_id])
     if @user
       erb :account
     else
